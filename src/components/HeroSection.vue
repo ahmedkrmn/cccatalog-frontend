8c2cd00205fbf07a1538c59d3c809a249c64b1a1
ahmedkrmn <20889958+ahmedkrmn@users.noreply.github.com>
2019-04-26 18:34:50 +0200

    Add filter drop-down for home page

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,26 +1,9 @@
 <template>
   <div class="hero">
     <img class="logo" src="../assets/cc-logo_large_black.png">
-    <form class="hero_search-form"
-          role="search"
-          method="post"
-          v-on:submit.prevent="onSubmit">
-      <div class="search-form_ctr grid-x">
-          <div class="cell large-12">
-            <input required="required"
-                   autofocus="true"
-                   class="hero_search-input"
-                   type="search"
-                   placeholder="Search the commons..."
-                   autocapitalize="none"
-                   id="searchTerm"
-                   v-model.lazy="form.searchTerm">
-          </div>
-          <div class="cell large-12">
-            <button class="hero_search-btn" title="Search"></button>
-          </div>
-      </div>
-    </form>
+    <div class="hero_search-form search-form_ctr">
+      <search-grid-form></search-grid-form>
+    </div>
     <div class="description">
         <p>
           Search for free content in the public domain and under Creative Commons licenses.
@@ -39,9 +22,13 @@
 
 <script>
 import { SET_QUERY } from '@/store/mutation-types';
+import SearchGridForm from '@/components/SearchGridForm';
 
 export default {
   name: 'hero-section',
+  components: {
+    SearchGridForm,
+  },
   data: () => ({ form: { searchTerm: '' } }),
   methods: {
     onSubmit() {
@@ -87,48 +74,6 @@ $hero-height: 75vh;
     width: 100%;
   }
 
-  .hero_search-input {
-    font-size: 24px;
-    padding-left: 30px;
-    margin-bottom: 0;
-    width: 100%;
-    height: 60px;
-    outline: 0;
-    border-radius: 3px;
-    border-width: 0;
-    box-shadow: none;
-  }
-
-  .hero_search-input::placeholder {
-    color: rgb(130, 130, 130);
-  }
-
-  .hero_search-btn {
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: calc( 100% - 3px );
-    width: 60px;
-    margin: 2px;
-    font-size: 24px;
-    cursor: pointer;
-    transition: all .2s ease-in-out;
-    border-radius: 3px;
-
-    &:after {
-      content: '';
-      background: url('../assets/search-icon_black.svg') center center no-repeat;
-      background-size: 20px;
-      opacity: 0.7;
-      top: 0;
-      left: 0;
-      bottom: 0;
-      right: 0;
-      position: absolute;
-      z-index: 10;
-    }
-  }
-
   &:before {
     content: '';
     position: absolute;
@@ -162,18 +107,30 @@ $hero-height: 75vh;
   .search-form_ctr {
     padding: 0 .9375rem;
   }
+  .logo {
+    height: 5.5em;
+  }
+}
+</style>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
-  .hero .hero_search-input {
+<style lang="scss">
+#app .search-form_input::placeholder {
+  color: rgb(130, 130, 130);
+}
+
+#app .search-form_search-button{
+  margin-left:auto; margin-right:0;
+}
+
+/* Small only */
+@media screen and (max-width: 40em) {
+  #app .search-form_input {
     font-size: 20px;
     padding-left: 15px;
   }
-
-  .hero .hero_search-btn {
+  #app .search-form_search-button {
     right: 10px;
-  }
-
-  .logo {
-    height: 5.5em;
   }
 }
 </style>
